125873fa360fd7575ce45886c821488991d0a6e3
Marko Jekrić <markojerkic266@gmail.com>
2022-12-24 17:40:14 +0100

    remove dist

--- dist/assets/async-881d355d.js
+++
@@ -1,1 +0,0 @@
-import{T as n,r as s,o as e,p as t}from"./index-9f663cba.js";const a=o=>{const r=n(()=>{s.value.lastNameId.routeTo({lastname:"jerkic"})},[]);return e(t,{children:e("div",{className:"bg-fuchsia-200",children:[e("p",{children:["Async rute: ",o.route]}),e("button",{className:"bg-blue-300",onClick:()=>r(),children:"Idemo na osobu jerkic"})]})})};export{a as AsyncComponent};
